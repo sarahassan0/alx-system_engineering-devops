700d6bafdc60e077a01473c7a6e35c6c2cd62724
sarahassan0 <sarraree5@gmail.com>
2023-08-01 22:54:43 +0300

    fix 0

--- 0x06-regular_expressions/0-simply_match_school.rb
+++ 0x06-regular_expressions/0-simply_match_school.rb
@@ -1,7 +1,2 @@
 #!/usr/bin/env ruby
-puts ARGV[0].scan(/127.0.0.[0-9]/).join
-sylvain@ubuntu$
-sylvain@ubuntu$ ./example.rb 127.0.0.2
-127.0.0.2
-sylvain@ubuntu$ ./example.rb 127.0.0.1
-127.0.0.1
+puts ARGV[0].scan(/School/).join
