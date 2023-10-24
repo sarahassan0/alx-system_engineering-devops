c696dc09f88b5a49160a251a7e0a72a64ed80350
sarahassan0 <sararee5@gmail.com>
2023-10-24 03:39:46 +0300

    faillllllllll

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -7,10 +7,9 @@ from sys import argv
 if __name__ == '__main__':
 
     url = 'https://jsonplaceholder.typicode.com/'
-    userId = argv[1]
 
-    user = requests.get(url+'users/'+userId).json()
-    todos = requests.get(f'{url}todos?userId={userId}').json()
+    user = requests.get(url+'users/'+argv[1]).json()
+    todos = requests.get(f'{url}todos?userId={argv[1]}').json()
     completed = [todo.get('title') for todo in todos if todo.get('completed')]
 
     print(f'Employee {user.get("name")} is done with tasks'
