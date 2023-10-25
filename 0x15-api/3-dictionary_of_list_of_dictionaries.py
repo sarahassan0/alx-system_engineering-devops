5a37e046d369712f8355b99e9eb344e2566d33d5
sarahassan0 <sararee5@gmail.com>
2023-10-25 04:07:23 +0300

    task 3

--- 0x15-api/3-dictionary_of_list_of_dictionaries.py
+++ 0x15-api/3-dictionary_of_list_of_dictionaries.py
@@ -13,3 +13,15 @@ if __name__ == '__main__':
     employees_tasks = {}
     for user in users:
         userId = user.get('id')
+        employee_tasks_list = []
+        for todo in todos:
+            if todo.get('userId') == userId:
+                employees_tasks[userId] = [
+                    {"username": user.get('username'),
+                     "task": todo.get('title'),
+                     "completed": todo.get('completed')}
+                    for todo in todos
+                ]
+
+    with open('todo_all_employees.json', mode='w') as file:
+        json.dump(employees_tasks, file)
