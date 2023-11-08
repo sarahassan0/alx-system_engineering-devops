a5cee2045429d1ee6b723792ef717ecfaa316e13
sarahassan0 <sararee5@gmail.com>
2023-11-08 03:54:09 +0200

    recurseve function

---
+++ 0x16-api_advanced/2-recurse.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python3
+"""Queries the Reddit API and prints the titles of all
+    hot posts listed for a given subreddit."""
+
+import requests
+
+
+def recurse(subreddit, hot_list=[], after=None, count=0):
+    """Queries the Reddit API and returns a list containing the titles
+        of all hot articles for a given subreddit"""
+
+    url = 'https://www.reddit.com/r/{}/hot.json?limit=100'.format(
+        subreddit)
+    params = {
+        'after': after,
+        'count': count
+    }
+    res = requests.get(url, headers={'User-Agent': 'anything'}, params=params,
+                       allow_redirects=False)
+
+    if res.status_code == 200:
+        data = res.json().get('data', {})
+        posts = data.get('children', [])
+        for post in posts:
+            hot_list.append(post.get('data', {}).get('title', ''))
+
+        after = data.get('after', '')
+        print(count)
+        count = len(hot_list[1:])
+        return recurse(subreddit, hot_list, after, count)
+    else:
+        return None
